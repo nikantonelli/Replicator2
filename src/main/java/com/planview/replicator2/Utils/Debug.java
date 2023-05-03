ad64898699e2065e86a52bec5ed241f4a9befe5e
nikantonelli <nik@nikantonelli>
2023-05-03 09:38:34 +0100

    Add time/date to debug messages

--- src/main/java/com/planview/replicator2/Utils/Debug.java
+++ src/main/java/com/planview/replicator2/Utils/Debug.java
@@ -1,5 +1,7 @@
 package com.planview.replicator2.Utils;
 
+import java.util.Date;
+
 public class Debug {
     public final static Integer ALWAYS = -1;
     public final static Integer ERROR = 0;
@@ -25,6 +27,7 @@ public class Debug {
     }
 
     public void p(Integer level, String fmt, Object... parms) {
+		String defaultString = "NOTE: ";
         String lp = null;
         switch (level) {
             case 2: {
@@ -48,9 +51,25 @@ public class Debug {
                 break;
             }
             default: {
-                lp = "NOTE: ";
+                lp = defaultString;
             }
         }
+		
+		//Switch on timestamps if we are debugging or verbose
+		switch (debugPrint) {
+			case 0:
+			case 1:
+			case 2: {
+				break;
+			}
+			case 3:
+			case 4: {
+				//Only print dates on non "NOTE" ones
+				if (!lp.equals(defaultString)) {
+					lp += new Date().toString() + " : ";
+				}
+			}
+		}
         if (level <= debugPrint) {
             System.out.printf(lp+fmt, parms);
         }
